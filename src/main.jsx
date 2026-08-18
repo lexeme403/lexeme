8bed6dcee71804d91d99a5f73cd6a432ba3fc8ab
shSalmanian <shireensalmanian@gmail.com>
2026-08-18 21:14:38 +0330

    Fix Vite production build

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 import "./styling/style.css";
+import "./styling/menu.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
