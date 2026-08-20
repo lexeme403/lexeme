8baccb6e8d126ce1608c81e3350296cd58ea482b
shSalmanian <shireensalmanian@gmail.com>
2026-08-20 20:59:15 +0330

    fix routing 404 bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
@@ -9,8 +9,8 @@ import "./styling/menu.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/lexeme">
+        <HashRouter>
             <App />
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
